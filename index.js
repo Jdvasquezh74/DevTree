d47335459406f4176cbe4c6e6aaed59117fa1917
Juan Vasquez <jd.vasquezh@uniandes.edu.co>
2025-01-06 20:52:28 -0500

    devDependencies, dev script, ESM type

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('Hola Mundo desde Devtree')
+import express from 'express'
+
+const app = express()
+
+app.get("/", (req, res) => {
+    res.send('Hola mundo en Express')
+})
+
+const port = process.env.PORT || 4000
+
+app.listen(port, () => {
+    console.log('Servidor funcionando en el puerto: ', port)
+})
