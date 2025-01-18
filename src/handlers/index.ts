c51b24af0ed37ad9ff69e8e3cf9d6bb8a6687bdf
Juan Vasquez <jd.vasquezh@uniandes.edu.co>
2025-01-18 13:37:37 -0500

    Hash para contraseñas y Handle único para nombres de usuario

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,18 +1,31 @@
 import type { Request, Response } from 'express'
+import slug from 'slug'
 import User from "../models/User"
+import { hashPassword } from '../utils/auth'
 
 export const createUser = async (req: Request, res: Response) : Promise<any> => {
 
-    const {email} = req.body
+    const {email, password} = req.body
 
     const userExists = await User.findOne({email})
 
     if (userExists) {
-        const error = new Error('El usuario ya está registrado')
+        const error = new Error('Un usuario con ese mail ya está registrado')
         return res.status(409).json({error : error.message})
     }
     
+    const handle = slug(req.body.handle, '')
+    const handleExists = await User.findOne({handle})
+
+    if (handleExists) {
+        const error = new Error('Nombre de usuario no disponible')
+        return res.status(409).json({error : error.message})
+    }
+    
+
     const user = new User(req.body)
+    user.password = await hashPassword(password)
+    user.handle = handle
 
     await user.save()
 
