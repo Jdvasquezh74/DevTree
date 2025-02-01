a47a8de6a10d7d15e1a19433274866726e190259
Juan Vasquez <jd.vasquezh@uniandes.edu.co>
2025-01-31 23:21:06 -0500

    Conexión entre backend y frontend con Axios

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,16 @@
 import express from 'express'
+import cors from 'cors'
 import router from './router'
 import { connectDB } from './config/db'
 import 'dotenv/config'
+import { corsConfig } from './config/cors'
+
+connectDB()
 
 const app = express()
 
-connectDB()
+//Cors
+app.use(cors(corsConfig))
 
 //Leer datos de formularios
 app.use(express.json())
